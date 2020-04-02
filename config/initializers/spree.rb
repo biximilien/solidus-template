f5e088e15e7260dec149e5d2e5701065ad339aba
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2020-04-02 14:50:11 -0400

    feat: Add custom logo and improve products page
    Add custom logo.
    Improve products page by reducing size of text and displaying 4 products
    per row.

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -31,7 +31,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus.svg"
+  config.admin_interface_logo = "solidus-template-logo.svg"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
